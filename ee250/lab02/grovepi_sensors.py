4e03d52ce6d04a0f84838897783fd266b8e0998b
Haowen Liu <liu.haowen.andy@gmail.com>
2020-08-26 15:48:41 -0700

    lab02 initial attempt

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -5,8 +5,8 @@ List team members here.
 Insert Github repository link here.
 """
 
-"""python3 interpreters in Ubuntu (and other linux distros) will look in a 
-default set of directories for modules when a program tries to `import` one. 
+"""python3 interpreters in Ubuntu (and other linux distros) will look in a
+default set of directories for modules when a program tries to `import` one.
 Examples of some default directories are (but not limited to):
   /usr/lib/python3.5
   /usr/local/lib/python3.5/dist-packages
@@ -23,16 +23,38 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grove_rgb_lcd import *
 
-"""This if-statement checks if you are running this python file directly. That 
-is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
+previous_display = ""
+
+
+def updateLCD(threshold, current):
+    global previous_display
+    display = str(threshold).rjust(3)
+    display += "cm"
+    if current < threshold:
+        display += " OBJ PRES\n"
+        setRGB(255, 0, 0)
+    else:
+        setRGB(0, 255, 0)
+    display += str(current).rjust(3) + "cm"
+    if display != previous_display:
+        setText_norefresh(display)
+        previous_display = display
+
+
+"""This if-statement checks if you are running this python file directly. That
+is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will
 be true"""
 if __name__ == '__main__':
-    PORT = 4    # D4
+    ultrasonic = 4  # D4
+    rotary = 0  # A0
 
     while True:
-        #So we do not poll the sensors too quickly which may introduce noise,
-        #sleep for a reasonable time of 200ms between each iteration.
+        # So we do not poll the sensors too quickly which may introduce noise,
+        # sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
 
-        print(grovepi.ultrasonicRead(PORT))
+        current = grovepi.ultrasonicRead(ultrasonic)
+        threshold = grovepi.analogRead(rotary)
+        updateLCD(threshold, current)
